0db6dbbbd5ee18b3a78432217b5b9e21368e9ce2
Joel Ajay <ajaijoel@gmail.com>
2025-09-18 23:24:28 +0530

    feat: store default values in config.yaml

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -15,8 +15,14 @@ import (
 
 func main() {
 	// Initialize Viper
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		log.Fatalf("Error reading config file, %s", err)
+	}
 	viper.AutomaticEnv()
-	viper.SetDefault("PORT", "8080")
 	port := viper.GetString("PORT")
 
 	// Initialize the database connection
