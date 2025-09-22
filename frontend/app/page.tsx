e6f37b36b2147e8629d410d1a55f47dea81c42c5
Joel Ajay <ajaijoel@gmail.com>
2025-09-22 17:15:51 +0530

    fix: yet another attempt at making refreshes work

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,35 +11,26 @@ import type { File } from "@/lib/types"
 export default function HomePage() {
   const [shareFile, setShareFile] = useState<File | null>(null)
   const [currentFolderId, setCurrentFolderId] = useState<string | undefined>()
-  const [refreshKey, setRefreshKey] = useState(0)
 
   const handleShareFile = (file: File) => {
     setShareFile(file)
   }
 
-  const handleUploadComplete = () => {
-    setRefreshKey((prev) => prev + 1)
-  }
-
-  const handleFileUpdate = () => {
-    setRefreshKey((prev) => prev + 1)
-  }
-
   return (
     <ProtectedRoute>
-      <DashboardLayout currentFolderId={currentFolderId} onUploadComplete={handleUploadComplete}>
+      <DashboardLayout currentFolderId={currentFolderId}>
         <Box>
           <Typography variant="h4" component="h1" gutterBottom>
             My Files
           </Typography>
-          <FileBrowser refresh={refreshKey} onShareFile={handleShareFile} />
+          <FileBrowser onShareFile={handleShareFile} />
         </Box>
 
         <ShareModal
           open={!!shareFile}
           onClose={() => setShareFile(null)}
           file={shareFile}
-          onFileUpdate={handleFileUpdate}
+          onFileUpdate={() => {}}
         />
       </DashboardLayout>
     </ProtectedRoute>
